9ba6d766ef22e4ba2eac61cb255fc6d62ecfabb0
Armando Rodriguez <arod1207@me.com>
2020-10-26 20:46:53 -0500

    added title for home page

---
+++ src/Components/CurrentProject/CurrentProject.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './CurrentProject.css';
+
+import RestaurantImage from '../Assets/Images/Restaurant-app.png';
+
+function CurrentProject() {
+    return <div className="currentProject">hi</div>;
+}
+
+export default CurrentProject;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
+
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__header">
+                In This World, Creations Are Limitless
+            </div>
+        </div>
+    );
+}
+
+export default Home;
