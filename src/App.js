8efd407b5593af72460742ece7fabcf3a1267231
Armando Rodriguez <arod1207@me.com>
2020-10-28 14:54:26 -0500

    added a contact page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import LosPelletsImage from './Components/Assets/Images/LosPellets.png';
 import MoodifiImage from './Components/Assets/Images/Moodifi.png';
 import WeatherImage from './Components/Assets/Images/weatherDashboard.png';
@@ -8,58 +11,68 @@ import EmployeeImage from './Components/Assets/Images/EmployeeDirectory.png';
 import Home from './Components/Home/Home';
 import CurrentProject from './Components/CurrentProject/CurrentProject';
 import Projects from './Components/Projects/Projects';
+import ContactMe from './Components/ContactMe/ContactMe';
 
 import './App.css';
 
 function App() {
     return (
         <div className="app">
-            <Home />
-            <CurrentProject />
-            <div className="projects">
-                <Projects
-                    title="Moodifi"
-                    image={MoodifiImage}
-                    link="https://shrouded-chamber-97830.herokuapp.com/"
-                    github="https://github.com/arod1207/Moodifi"
-                    description="Curated playlist for whatever you do, and your moods. Discovery new music based on your activities and feelings! Browse, rate and listen all within one app!"
-                />
-                <Projects
-                    title="Los Pellets Guapos"
-                    image={LosPelletsImage}
-                    link="http://los-pellets-guapos.herokuapp.com/"
-                    github="https://github.com/arod1207/Los-Pellets-Guapos"
-                    description="Los Pellets Guapos is a pellet grill sensor application that automatically tracks the amount of pellets remaining. The software features secured login, recipe finders, and mobile priority design."
-                />
-                <Projects
-                    title="Weather App"
-                    image={WeatherImage}
-                    link="https://arod1207.github.io/Weather-Dashboard/"
-                    github="https://github.com/arod1207/Weather-Dashboard"
-                    description="This weather dashboard utilizes the OpenWeather API in order to retrieve data from different cities. The last user search is then saved into local storage. The current forcast is displayed, as well as, a 5 day forcast that is updated in three hour intervals. UV index is highlighted in various colors to inform the user of the UV exposure for that day."
-                />
-                <Projects
-                    title="Google Book Store"
-                    image={GoogleBookImage}
-                    link="https://google-book-search-w-react.herokuapp.com/"
-                    github="https://github.com/arod1207/Google-Book-Search"
-                    description="This is a React-based Google Books Search app. It is a full stack MERN applications that uses React on the client-side and Node, Express and MongoDB on the back-end. The user has the ability to save books using MongoDB Atlas."
-                />
-                <Projects
-                    title="Work Day Scheduler"
-                    image={WorkDayImage}
-                    link="https://arod1207.github.io/Work-Day-Scheduler/"
-                    github="https://github.com/arod1207/Work-Day-Scheduler"
-                    description="A simple calendar application that allows the user to save events for each hour of the day."
-                />
-                <Projects
-                    title="Employee Directory"
-                    image={EmployeeImage}
-                    link="https://frozen-badlands-93210.herokuapp.com/"
-                    github="https://github.com/arod1207/HW-19-Employee-Directory"
-                    description="A react app that employees can use to view non-sensitive data about other employees. Names can also be sorted by alphabetical order."
-                />
-            </div>
+            <Router>
+                <Switch>
+                    <Route path="/contactme">
+                        <ContactMe />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                        <CurrentProject />
+                        <div className="projects">
+                            <Projects
+                                title="Moodifi"
+                                image={MoodifiImage}
+                                link="https://shrouded-chamber-97830.herokuapp.com/"
+                                github="https://github.com/arod1207/Moodifi"
+                                description="Curated playlist for whatever you do, and your moods. Discovery new music based on your activities and feelings! Browse, rate and listen all within one app!"
+                            />
+                            <Projects
+                                title="Los Pellets Guapos"
+                                image={LosPelletsImage}
+                                link="http://los-pellets-guapos.herokuapp.com/"
+                                github="https://github.com/arod1207/Los-Pellets-Guapos"
+                                description="Los Pellets Guapos is a pellet grill sensor application that automatically tracks the amount of pellets remaining. The software features secured login, recipe finders, and mobile priority design."
+                            />
+                            <Projects
+                                title="Weather App"
+                                image={WeatherImage}
+                                link="https://arod1207.github.io/Weather-Dashboard/"
+                                github="https://github.com/arod1207/Weather-Dashboard"
+                                description="This weather dashboard utilizes the OpenWeather API in order to retrieve data from different cities. The last user search is then saved into local storage. The current forcast is displayed, as well as, a 5 day forcast that is updated in three hour intervals. UV index is highlighted in various colors to inform the user of the UV exposure for that day."
+                            />
+                            <Projects
+                                title="Google Book Store"
+                                image={GoogleBookImage}
+                                link="https://google-book-search-w-react.herokuapp.com/"
+                                github="https://github.com/arod1207/Google-Book-Search"
+                                description="This is a React-based Google Books Search app. It is a full stack MERN applications that uses React on the client-side and Node, Express and MongoDB on the back-end. The user has the ability to save books using MongoDB Atlas."
+                            />
+                            <Projects
+                                title="Work Day Scheduler"
+                                image={WorkDayImage}
+                                link="https://arod1207.github.io/Work-Day-Scheduler/"
+                                github="https://github.com/arod1207/Work-Day-Scheduler"
+                                description="A simple calendar application that allows the user to save events for each hour of the day."
+                            />
+                            <Projects
+                                title="Employee Directory"
+                                image={EmployeeImage}
+                                link="https://frozen-badlands-93210.herokuapp.com/"
+                                github="https://github.com/arod1207/HW-19-Employee-Directory"
+                                description="A react app that employees can use to view non-sensitive data about other employees. Names can also be sorted by alphabetical order."
+                            />
+                        </div>
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
---
+++ src/Components/ContactMe/ContactMe.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import ScrollAnimation from 'react-animate-on-scroll';
+
+import './ContactMe.css';
+
+function ContactMe() {
+    return (
+        <div className="contactMe">
+            <div className="contactMe__card">
+                <div className="contactMe__Header">
+                    <ScrollAnimation
+                        duration={3}
+                        delay={1000}
+                        animateIn="hinge"
+                        initiallyVisible={true}
+                    >
+                        <h1>Contact Me</h1>
+                    </ScrollAnimation>
+                </div>
+                <div className="contactMe__LinkedIn contactMe__options ">
+                    <a href="https://www.linkedin.com/in/armandorodriguez7/">
+                        <i class="fab fa-linkedin fa-3x"></i>
+                    </a>
+                </div>
+                <div className="contactMe__Email contactMe__options ">
+                    <p>
+                        <a href="mailto:arod1207@me.com">
+                            <i class="fas fa-envelope fa-3x"></i>
+                        </a>
+                    </p>
+                </div>
+                <div className="contactMe__PhoneNumber contactMe__options ">
+                    (210)999-1191
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ContactMe;
